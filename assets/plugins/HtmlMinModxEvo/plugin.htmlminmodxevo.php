b0dc1f6ca64fde34848e068c13b89a919b01d0a6
ProjectSoft <projectsoft2009@yandex.ru>
2016-07-03 08:47:51 +0400

    Update plugin.htmlminmodxevo.php

--- assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
+++ assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
@@ -1,16 +1,23 @@
 <?php
-/**************************************/
-/** HtmlMinModxEvo plugin for MODX Evo
-*
-* @version 1.0
-* @author ProjectSoft (projectsoft@ioweb.ru)
-*
-* @category plugin
-* @internal @event OnWebPagePrerender
-* @internal @modx_category HTML-code
-*
+/**
+ * HtmlMinModxEvo
+ * HtmlMinModxEvo plugin for MODX Evo
+ *
+ * Порт плагина HtmlInLine https://github.com/MODXim/MODX.plugins/tree/master/HtmlInLine
+ * Добавлена более удобная регулярка удаления лишних пробелов, переносов табов.
+ * Плюс бонус - удаление комментариев в html коде, но при этом оставляем комментарии для браузера IE
+ *
+ * @category    plugin 
+ * @version 1.0
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL) 
+ * @internal    @properties 
+ * @internal    @events OnWebPagePrerender 
+ * @internal    @modx_category Content 
+ * @internal    @legacy_names HtmlInLine
+ * @internal    @installset base
+ * @author ProjectSoft (projectsoft@ioweb.ru)
 */
-/**************************************/
+//author ProjectSoft (projectsoft@ioweb.ru)
 if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 global $modx;
 $e =&$modx->event;
