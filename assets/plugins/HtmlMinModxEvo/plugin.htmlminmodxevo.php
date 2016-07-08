4e79a89affb2649a427bfa364c9505d397076026
ProjectSoft <projectsoft2009@yandex.ru>
2016-07-08 04:08:48 +0400

    :ru: Обновлён

--- assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
+++ assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
@@ -3,6 +3,7 @@
  * HtmlMinModxEvo
  * HtmlMinModxEvo plugin for MODX Evo
  *
+ * 
  * Порт плагина HtmlInLine https://github.com/MODXim/MODX.plugins/tree/master/HtmlInLine
  * Добавлена более удобная регулярка удаления лишних пробелов, переносов табов.
  * Плюс бонус - удаление комментариев в html коде, но при этом оставляем комментарии для браузера IE
