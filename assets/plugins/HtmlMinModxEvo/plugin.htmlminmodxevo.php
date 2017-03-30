8f48a5d3e14c88c481223aee54d63a515e7ac418
ProjectSoft <projectsoft2009@yandex.ru>
2017-03-31 01:09:06 +0400

    fix replase multiline meta description

--- assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
+++ assets/plugins/HtmlMinModxEvo/plugin.htmlminmodxevo.php
@@ -21,11 +21,22 @@
 //author ProjectSoft (projectsoft@ioweb.ru)
 if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 global $modx;
+if (!function_exists('replace_project_html')) {
+	function replace_project_html($matches){
+		$res = preg_replace('(\r(?:\n)?)', "\xD6\xD6\xD6\xD6", $matches[2]);
+		return $matches[1].'"'.$res.'"';
+	}
+}
 $e =&$modx->event;
 switch ($e->name) {
     case "OnWebPagePrerender":{
         if($modx->documentObject['searchable']==1){
-            $modx->documentOutput = preg_replace('|\s+|', ' ', preg_replace('|(\s+)?\n(\s+)?|', '', preg_replace('|<!(--)?(\s+)?(?!\[).*-->|', '', $modx->documentOutput)));
+			$str = $modx->documentOutput;
+			$re = '/((?:content=)|(?:"description":\s+))(?:"|\')([A-я\S\s\d\D\X\W\w]+)(?:"|\')/mUi';
+			$str = preg_replace_callback($re,"replace_project_html",$str);
+			$str = preg_replace("/(\xD6\xD6\xD6\xD6)/", "\n", preg_replace('|\s+|', ' ', preg_replace('|(\s+)?\n(\s+)?|', '', preg_replace('|<!(--)?(\s+)?(?!\[).*-->|', '', $str))));
+			
+           	$modx->documentOutput = $str;
         }
         break;
     }
